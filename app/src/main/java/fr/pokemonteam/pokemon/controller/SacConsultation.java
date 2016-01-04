27bca27709c47e029674db8176c99690146cc7c0
melvin <melvinmasdieu@gmail.com>
2016-01-04 15:34:56 +0100

    sac consultation in progress

---
+++ app/src/main/java/fr/pokemonteam/pokemon/controller/SacConsultation.java
@@ -0,0 +1,15 @@
+package fr.pokemonteam.pokemon.controller;
+
+/**
+ * Created by melvin on 1/4/16.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+
+import fr.pokemonteam.pokemon.bdd.Database;
+
+public class SacConsultation extends AppCompatActivity {
+
+    Database db;
+
+}
